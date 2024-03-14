c59b8e6cc31a66678fd79cc2b74426833ee42d6d
markshawn2020 <shawninjuly@gmail.com>
2024-03-14 14:20:01 +0800

    :sparkles: fetch_data_and_emit

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -1,8 +1,58 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::ffi::c_int;
+use reqwest;
+use std::sync::mpsc::{self, TryRecvError};
+use std::sync::{Arc, Mutex};
+use std::thread;
+use serde_json::json;
+use tauri::{AppHandle, command, Manager};
+use threadpool::ThreadPool;
+use tokio::runtime::Runtime;
+
+struct DirItem {
+    size: c_int,
+    is_dir: bool,
+    last_modified: String,
+    folder_path: String,
+    folder_name: String,
+}
+
+struct ListData {
+    dirent_list: Vec<DirItem>,
+}
+
+async fn fetch_data() -> Result<String, reqwest::Error> {
+    println!("fetching data...");
+    let response = reqwest::get("https://cloud.tsinghua.edu.cn/api/v2.1/share-links/689824200edb49888695/dirents?path=/")
+        .await?
+        .text()
+        .await?;
+    Ok(response)
+}
+
+#[command]
+fn fetch_data_and_emit(app: AppHandle) {
+    println!("fetching data and emit...");
+
+    let app_clone = app.clone();
+    // new tokio runtime
+    let rt = Runtime::new().unwrap();
+    rt.block_on(async move {
+        match fetch_data().await {
+            Ok(data) => {
+                println!("emitting: {}", data);
+                let _ = app_clone.emit_all("list_data", json!({"data": data}));
+            }
+            Err(e) => eprintln!("Error fetching data {:?}", e)
+        }
+    })
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![fetch_data_and_emit])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,37 +1,99 @@
 "use client";
 
-import React, { useState } from "react";
-import { Input } from "../components/ui/input";
 import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
+import { listen } from "@tauri-apps/api/event";
+import { invoke } from "@tauri-apps/api/tauri";
+import React, { useEffect, useState } from "react";
+import { Input } from "../components/ui/input";
+
+type DirItem = {
+  size: number;
+  last_modified: string;
+} & (
+  | {
+      is_dir: true;
+      folder_path: string;
+      folder_name: string;
+    }
+  | {
+      is_dir: false;
+      file_name: string;
+      file_path: string;
+    }
+);
+
+type ListDirResult = {
+  dirent_list?: DirItem[];
+};
 
 export default function Home() {
-  const [input, setInput] = useState("");
+  const [dirs, setDirs] = useState<DirItem[]>([]);
+
+  useEffect(() => {
+    const unListen = listen<{ data: string }>("list_data", (event) => {
+      console.log("Data from Rust: ", event.payload);
+
+      const data = JSON.parse(event.payload.data) as ListDirResult;
+      console.log("Data parsed: ", data);
+      if (data.dirent_list) setDirs(data.dirent_list);
+    });
+
+    return () => {
+      unListen.then((fn) => fn());
+    };
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className={"flex flex-col gap-4 items-center"}>
         <div className={"text-2xl"}>清华云下载器</div>
 
-        <div className={"flex items-center gap-4"}>
-          <Input
-            value={input}
-            onChange={(event) => {
-              setInput(event.currentTarget.value);
-            }}
-          />
-          <Button
-            size={"sm"}
-            className={"px-8"}
-            variant={"secondary"}
-            onClick={() => {
-              toast.info(`input: ${input}`);
-            }}
-          >
-            解析
-          </Button>
-        </div>
+        <InputLine />
+
+        <DirLists dirs={dirs} />
       </div>
     </main>
   );
 }
+
+const InputLine = () => {
+  const [input, setInput] = useState("");
+
+  const trigger = async () => {
+    await invoke<string>("fetch_data_and_emit");
+  };
+
+  return (
+    <div className={"flex items-center gap-4"}>
+      <Input
+        value={input}
+        onChange={(event) => {
+          setInput(event.currentTarget.value);
+        }}
+      />
+      <Button
+        size={"sm"}
+        className={"px-8"}
+        variant={"secondary"}
+        onClick={trigger}
+      >
+        解析
+      </Button>
+    </div>
+  );
+};
+
+const DirLists = ({ dirs }: { dirs: DirItem[] }) => {
+  return (
+    <div className={"flex flex-col gap-2"}>
+      <div>条目列表</div>
+
+      {dirs.map((dir, index) => (
+        <div key={index}>
+          <span>isDir: {dir.is_dir ? "TRUE" : "FALSE"}</span>
+          <span> {dir.is_dir ? dir.folder_path : dir.file_path}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
