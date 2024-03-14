96b67e2a9c4570f8068ef8dad068742e41ec7134
markshawn2020 <shawninjuly@gmail.com>
2024-03-15 04:12:58 +0800

    multiple consumers

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -1,13 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::ffi::c_int;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread::sleep;
 use std::time::Duration;
 
+use futures_util::future::join_all;
 use serde_json::{from_str, json};
 use tauri::{AppHandle, command, Manager, State};
+use tokio::sync::broadcast;
+use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 use utils::download_file;
 
@@ -17,8 +21,50 @@ mod schema;
 mod utils;
 
 
+async fn producer(app: AppHandle, repo: String, root_path: String, stop_signal: Arc<AtomicBool>, tx: broadcast::Sender<(String, c_int)>) {
+    let mut paths = vec![root_path.clone()];
+    let mut index = 0;
+
+    while let Some(path) = paths.pop() {
+        if (stop_signal.load(Ordering::SeqCst)) {
+            println!("stopped since interrupted");
+            break;
+        }
+
+        match utils::fetch_dir_list(repo.clone(), path.clone()).await {
+            Ok(data) => {
+                // println!("emitting: {}", data);
+                let _ = app.emit_all("list_data", json!({"children": &data, "parent": &path}));
+
+                for item in &data {
+                    if let Some(fp) = &item.folder_path {
+                        paths.push(fp.clone());
+                    } else if let Some(fp) = &item.file_path {
+                        tx.send((fp.clone(), index)).expect("TODO: panic message");
+                        index += 1;
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("Error fetching data: {}", e)
+            }
+        }
+
+        tokio::time::sleep(Duration::from_millis(100)).await;
+    }
+}
+
+async fn consumer(store_path: String, repo: String, path: String) {
+    download_file(store_path.clone(), repo.clone(), path.clone()).await.expect("TODO: panic message");
+}
+
 /***
 todo: static
+
+ref:
+- https://chat.openai.com/c/cfa0aba4-16c4-4aea-9e6f-4bba0e56a507
+- https://chat.openai.com/c/d428f113-52d1-408d-9986-52abc20ab04a
+
  */
 #[command]
 async fn fetch_data_and_emit(
@@ -33,49 +79,28 @@ async fn fetch_data_and_emit(
     let stop_signal = Arc::clone(&stop_signal);
     stop_signal.store(false, Ordering::SeqCst);
 
-    let (tx, mut rx) = tokio::sync::mpsc::channel(120);
+    let (tx, _): (broadcast::Sender<(String, c_int)>, broadcast::Receiver<(String, c_int)>) = broadcast::channel(120);
 
-    let mut paths = vec![root_path.clone()];
+    let n = 5;
+    for i in 0..n {
+        let mut rx = tx.subscribe();
+        let repo = repo.clone();
+        let store_path = store_path.clone();
 
-    let repo_clone = repo.clone();
+        tokio::spawn(async move {
+            while let Ok((path, index)) = rx.recv().await {
+                if (index % n == i) {
+                    consumer(store_path.clone(), repo.clone(), path).await;
+                }
+            }
+        });
+    }
+
 
     tokio::spawn(async move {
-        while let Some(path) = paths.pop() {
-            if (stop_signal.load(Ordering::SeqCst)) {
-                println!("stopped since interrupted");
-                break;
-            }
+        producer(app.clone(), repo.clone(), root_path.clone(), stop_signal, tx).await;
+    }).await.unwrap();
 
-            match utils::fetch_dir_list(repo.clone(), path.clone()).await {
-                Ok(data) => {
-                    // println!("emitting: {}", data);
-                    let _ = app.emit_all("list_data", json!({"children": &data, "parent": &path}));
-
-                    for item in &data {
-                        if let Some(fp) = &item.folder_path {
-                            paths.push(fp.clone());
-                        } else if let Some(fp) = &item.file_path {
-                            tx.send(fp.clone()).await.expect("TODO: panic message");
-                        }
-                    }
-                }
-                Err(e) => {
-                    eprintln!("Error fetching data: {}", e)
-                }
-            }
-
-            tokio::time::sleep(Duration::from_millis(100)).await;
-        }
-
-        tx.send("DONE".parse().unwrap()).await.unwrap();
-    });
-
-    while let Some(path) = rx.recv().await {
-        if path == "DONE" {
-            break;
-        }
-        download_file(store_path.clone(), repo_clone.clone(), path.clone()).await.expect("TODO: panic message");
-    }
 
     Ok(())
 }
