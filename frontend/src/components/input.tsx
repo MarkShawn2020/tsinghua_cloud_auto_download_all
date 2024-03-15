09ab7dfcaa9976bcdf65c90922f6e530e3c5f159
markshawn2020 <shawninjuly@gmail.com>
2024-03-15 08:35:08 +0800

    fixed search

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -15,8 +15,7 @@ export const InputLine = () => {
   const [input, setInput] = useState("");
   const [fetching, setFetching] = useAtom(fetchingAtom);
   const [storePath] = useAtom(storePathAtom);
-  const [rootDir, setRootDir] = useAtom(storePathAtom);
-  const [threadsCount, setThreadsCount] = useAtom(threadsCountAtom);
+  const [threadsCount] = useAtom(threadsCountAtom);
 
   const stopFetching = async () => {
     setFetching(false);
@@ -35,6 +34,7 @@ export const InputLine = () => {
   return (
     <div className={"w-full flex items-center gap-4 shrink-0"}>
       <Input
+        placeholder={"https://cloud.tsinghua.edu.cn/d/xxx/?p=yyy&mode=list"}
         value={input}
         onChange={(event) => {
           setInput(event.currentTarget.value);
@@ -45,11 +45,23 @@ export const InputLine = () => {
         className={"px-8"}
         variant={"secondary"}
         onClick={async () => {
+          if (!storePath) return toast.error("请先设置好存储位置");
+
           if (!fetching) {
+            const url = new URL(input);
+
+            const m = url.pathname.match("/d/(.*)/");
+            const repo = m ? m[1] : null;
+
+            const rootPath = url.searchParams.get("p") || "/";
+            console.log({ repo, rootPath });
+
+            if (!repo) return toast.error("网址不合法");
+
             setFetching(true);
             const args = {
-              repo: "689824200edb49888695",
-              rootPath: "/",
+              repo,
+              rootPath,
               storePath,
               n: threadsCount,
             };
@@ -59,66 +71,10 @@ export const InputLine = () => {
           } else void stopFetching();
         }}
       >
-        {fetching ? "停止下载" : "开始下载"}
+        {fetching ? "停止下载" : "确认"}
       </Button>
 
-      <Dialog>
-        <DialogTrigger>
-          <div
-            className={
-              "p-2 bg-muted rounded-lg hover:bg-muted/75 cursor-pointer"
-            }
-          >
-            <Settings />
-          </div>
-        </DialogTrigger>
-
-        <DialogContent className={"p-12"}>
-          <Card>
-            <CardHeader>
-              <CardTitle>配置项</CardTitle>
-            </CardHeader>
-
-            <CardContent className={"space-y-8"}>
-              <LabelLine title={"存储位置"}>
-                <div className={"relative  w-full h-full hover:underline"}>
-                  {rootDir ?? "暂无"}
-                  <Input
-                    contentEditable={false}
-                    className={
-                      "truncate cursor-pointer opacity-0 absolute inset-0 w-full h-full"
-                    }
-                    onClick={async () => {
-                      const selected = await open({
-                        directory: true,
-                        multiple: false,
-                        title: "默认存储位置",
-                      });
-
-                      if (Array.isArray(selected))
-                        return toast.error("不支持多个文件夹");
-                      else if (selected === null) return;
-
-                      setRootDir(selected);
-                    }}
-                  />
-                </div>
-              </LabelLine>
-
-              <LabelLine title={"线程数"}>
-                <Input
-                  min={1}
-                  type={"number"}
-                  value={threadsCount.toString()}
-                  onChange={(event) => {
-                    setThreadsCount(Number(event.currentTarget.value));
-                  }}
-                />
-              </LabelLine>
-            </CardContent>
-          </Card>
-        </DialogContent>
-      </Dialog>
+      <Config />
     </div>
   );
 };
@@ -133,5 +89,68 @@ export const LabelLine = ({
 
       <div className={"grow"}>{children}</div>
     </div>
+  );
+};
+
+export const Config = () => {
+  const [rootDir, setRootDir] = useAtom(storePathAtom);
+  const [threadsCount, setThreadsCount] = useAtom(threadsCountAtom);
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div
+          className={"p-2 bg-muted rounded-lg hover:bg-muted/75 cursor-pointer"}
+        >
+          <Settings />
+        </div>
+      </DialogTrigger>
+
+      <DialogContent className={"p-12"}>
+        <Card>
+          <CardHeader>
+            <CardTitle>配置项</CardTitle>
+          </CardHeader>
+
+          <CardContent className={"space-y-8"}>
+            <LabelLine title={"存储位置"}>
+              <div className={"relative  w-full h-full hover:underline"}>
+                {rootDir ?? "暂无"}
+                <Input
+                  contentEditable={false}
+                  className={
+                    "truncate cursor-pointer opacity-0 absolute inset-0 w-full h-full"
+                  }
+                  onClick={async () => {
+                    const selected = await open({
+                      directory: true,
+                      multiple: false,
+                      title: "默认存储位置",
+                    });
+
+                    if (Array.isArray(selected))
+                      return toast.error("不支持多个文件夹");
+                    else if (selected === null) return;
+
+                    setRootDir(selected);
+                  }}
+                />
+              </div>
+            </LabelLine>
+
+            <LabelLine title={"线程数"}>
+              <Input
+                min={1}
+                type={"number"}
+                value={threadsCount.toString()}
+                onChange={(event) => {
+                  setThreadsCount(Number(event.currentTarget.value));
+                }}
+              />
+            </LabelLine>
+          </CardContent>
+        </Card>
+      </DialogContent>
+    </Dialog>
   );
 };
